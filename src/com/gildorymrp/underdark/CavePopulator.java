0d36e0871c706c809dd6d56b45cf84efc2a0b6c0
Lucariatias <lucariatias@gmail.com>
2013-06-30 18:15:42 +0100

    Use simplex noise rather than Perlin noise when generating caves

--- src/com/gildorymrp/underdark/CavePopulator.java
+++ src/com/gildorymrp/underdark/CavePopulator.java
@@ -6,7 +6,7 @@ import org.bukkit.Chunk;
 import org.bukkit.Material;
 import org.bukkit.World;
 import org.bukkit.generator.BlockPopulator;
-import org.bukkit.util.noise.PerlinNoiseGenerator;
+import org.bukkit.util.noise.SimplexOctaveGenerator;
 
 public class CavePopulator extends BlockPopulator {
 
@@ -15,15 +15,16 @@ public class CavePopulator extends BlockPopulator {
 		int worldChunkX = source.getX() * 16;
 		int worldChunkZ = source.getZ() * 16;
 		int x, y, z;
-		PerlinNoiseGenerator noiseGenerator = new PerlinNoiseGenerator(world);
+		SimplexOctaveGenerator noiseGenerator = new SimplexOctaveGenerator(world, 8);
+		noiseGenerator.setScale(1 / 32.0);
 		for (x = worldChunkX; x < worldChunkX + 16; x++) {
 			for (z = worldChunkZ; z < worldChunkZ + 16; z++) {
-				for (y = world.getMaxHeight() - 32; y > world.getMaxHeight() - (32 + (noiseGenerator.noise(x, z) * 4)); y--) {
+				for (y = world.getMaxHeight() - 32; y > world.getMaxHeight() - (32 + (noiseGenerator.noise(x, z, 0.5, 0.5) * 4)); y--) {
 					if (world.getBlockAt(x, y, z).getType() != Material.AIR) {
 						world.getBlockAt(x, y, z).setType(Material.AIR);
 					}
 				}
-				for (y = world.getMaxHeight() - 32; y < world.getMaxHeight() - (32 - (noiseGenerator.noise(x, z) * 4)); y++) {
+				for (y = world.getMaxHeight() - 32; y < world.getMaxHeight() - (32 - (noiseGenerator.noise(x, z, 0.5, 0.5) * 4)); y++) {
 					if (world.getBlockAt(x, y, z).getType() != Material.AIR) {
 						world.getBlockAt(x, y, z).setType(Material.AIR);
 					}
